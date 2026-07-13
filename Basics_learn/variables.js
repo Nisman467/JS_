c37e19dd8ac3e1293c3befa69723f39ca39e6fe9
Nisman467 <shrestha.nisman@gmail.com>
2026-07-13 16:50:51 +0000

    first

---
+++ Basics_learn/variables.js
@@ -0,0 +1,10 @@
+const accountID = 2081047
+let accountEmail = "neuron@google.com"
+var accountPassword = "54321"
+accountCity = "Kathmandu"
+
+// accountID = 123434
+
+console.log(accountID)
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
