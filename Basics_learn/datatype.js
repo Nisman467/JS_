e0a3e8053cb93f4dac3e52ec478335861c996f92
Nisman467 <shrestha.nisman@gmail.com>
2026-07-15 03:34:13 +0000

    datatypes

---
+++ Basics_learn/datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Neuron") we are using the nodejs, not browser
+
+console.log("Neuron"); console.log(3 + 3)
+// readability is important so dont use a semicolon and write the code in same line 
+
+// datatypes
+let name = "Neuron"
+let age = 16
+let isLoggedIn = true
+let state
+let number = null
+/*
+number => range 2^(53)
+bigint => for bigger numbers
+string => ""
+boolen => true/false
+null => standalone value (object)
+undefine => undefine type
+symbol => unique
+*/ 
+
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
