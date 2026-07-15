f2078928480c290e8fea7bebd5953f75cc6a2563
Nisman467 <shrestha.nisman@gmail.com>
2026-07-15 20:21:41 +0545

    Update comparisonDatatype.js

--- Basics_learn/comparisonDatatype.js
+++ Basics_learn/comparisonDatatype.js
@@ -1,27 +1,27 @@
-// let value1, value2
-// value1 = 3
-// value2 = 1
-// console.log(value1 > value2);
-// console.log(value1 >= value2);
-// console.log(value1 < value2);
-// console.log(value1 <= value2);
-// console.log(value1 == value2);
-// console.log(value1 != value2);
+let value1, value2
+value1 = 3
+value2 = 1
+console.log(value1 > value2);
+console.log(value1 >= value2);
+console.log(value1 < value2);
+console.log(value1 <= value2);
+console.log(value1 == value2);
+console.log(value1 != value2);
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+console.log("2" > 1);
+console.log("02" > 1);
 
-// console.log(null > 0);
-// console.log(null < 0);
-// console.log(null == 0);
-// console.log(null != 0);
-// console.log(null >= 0);
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log(null != 0);
+console.log(null >= 0);
 
-// console.log(undefined > 0);
-// console.log(undefined < 0);
-// console.log(undefined == 0);
-// console.log(undefined != 0);
-// console.log(undefined >= 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined != 0);
+console.log(undefined >= 0);
 
 // *Note: Above conversion must be avoided.
 
