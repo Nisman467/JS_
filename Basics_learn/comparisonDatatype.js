48fdb4d849dad60781d104de868dcb07f38a2b07
Nisman467 <shrestha.nisman@gmail.com>
2026-07-15 14:35:23 +0000

    comparison_datatype

---
+++ Basics_learn/comparisonDatatype.js
@@ -0,0 +1,30 @@
+// let value1, value2
+// value1 = 3
+// value2 = 1
+// console.log(value1 > value2);
+// console.log(value1 >= value2);
+// console.log(value1 < value2);
+// console.log(value1 <= value2);
+// console.log(value1 == value2);
+// console.log(value1 != value2);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null != 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined != 0);
+// console.log(undefined >= 0);
+
+// *Note: Above conversion must be avoided.
+
+// === (strict check) also check the datatypes
+console.log("2" == 2);
+console.log("2" === 2);
