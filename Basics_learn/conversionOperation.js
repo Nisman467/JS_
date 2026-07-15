9328a8c012848357b49cc6d4c7c251dc05bd3328
Nisman467 <shrestha.nisman@gmail.com>
2026-07-15 14:21:36 +0000

    conversionOperations_v2

--- Basics_learn/conversionOperation.js
+++ Basics_learn/conversionOperation.js
@@ -62,3 +62,38 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+// ----------------Operations-----------------
+let value = 21
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%2)
+
+let str1 = "My"
+let str2 = " boy"
+let str3 = " Neuron"
+console.log(str1 + str2 + str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + 2 + "2" + 3);
+
+console.log(+true)
+console.log(+"")
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 1+3
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
