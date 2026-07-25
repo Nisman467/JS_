ae10b84519b9f9addef80682e4f9a80c501d0dbe
Nisman467 <shrestha.nisman@gmail.com>
2026-07-25 21:03:44 +0545

    Update array.js

--- 02_Basics/array.js
+++ 02_Basics/array.js
@@ -1,34 +1,34 @@
-// // array can be declared as variables or object using new
+// array can be declared as variables or object using new
 
-// const arr = [0, 1, 2, 3, 4, 5]
+const arr = [0, 1, 2, 3, 4, 5]
 
-// console.log(arr[0]);
+console.log(arr[0]);
 
 
 const bikes = new Array("Ducati", "Kawasaki", "CF MOTO", "Yamaha")
 
-// console.log(bikes[1]);
+console.log(bikes[1]);
 
 
-// //Array Method
-// bikes.push("Honda")
-// bikes.push("Puslar")
-// console.log(bikes); //array bikes is changed
+//Array Method
+bikes.push("Honda")
+bikes.push("Puslar")
+console.log(bikes); //array bikes is changed
 
-// bikes.pop()
-// console.log(bikes);
+bikes.pop()
+console.log(bikes);
 
-// bikes.unshift("Hyundai") //unshift() -> adds the data at initial position of the array i.e. bikes[0]
-// console.log(bikes);
+bikes.unshift("Hyundai") //unshift() -> adds the data at initial position of the array i.e. bikes[0]
+console.log(bikes);
 
-// bikes.shift() //removes the first elements of array
-// console.log(bikes);
+bikes.shift() //removes the first elements of array
+console.log(bikes);
 
-// console.log(bikes.includes(0)); //includes return true or false; whether that array includes or not 
-// console.log(bikes.includes("Ducati")); 
+console.log(bikes.includes(0)); //includes return true or false; whether that array includes or not 
+console.log(bikes.includes("Ducati")); 
 
-// console.log(bikes.indexOf("Ducati")); 
-// console.log(bikes.indexOf("Yamaha")); 
+console.log(bikes.indexOf("Ducati")); 
+console.log(bikes.indexOf("Yamaha")); 
 
 
 const newVehicles = bikes.join() //this joins the array as well as it changes the type into string 
